18bba43e52da7f12784ab054f872f03f7c2c7c9d
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-25 14:55:41 -0500

    Mi primer server con ExpressJs

--- express_basic/app.js
+++ express_basic/app.js
@@ -1,1 +1,15 @@
+// Usando objeto express
+const express = require('express')
+// App express y puerto por el que se escucha el server
+const app = express()
+const port = 3000
 
+// push inicial, responderá a la url localhost:3000
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+// Para inicializar la app
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
